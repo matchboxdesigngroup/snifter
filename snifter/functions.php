ed56e86252134d8ad130af97e728e2b7b09676f4
Dan Holloran <dtholloran@gmail.com>
2014-10-26 09:03:27 -0700

    Adds bas include logic for all includes

--- snifter/functions.php
+++ snifter/functions.php
@@ -9,3 +9,19 @@
  *
  * @author       Matchbox Design Group <info@matchboxdesigngroup.com>
  */
+
+$directories             = array();
+
+foreach ( $directories as $directory => $files ) {
+	if ( empty( $files ) ) continue;
+
+	foreach ( $files as $file ) {
+		$has_php  = ( strpos( $file, '.php' ) !== false );
+		$file     = ( $has_php ) ? $file : "{$file}.php";
+		$template = locate_template( "/{$directory}/{$file}" );
+
+		if ( $template == '' ) continue;
+
+		require_once $template;
+	} // foreach()
+} // foreach()
