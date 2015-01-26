39c6aafadfc8b27808bb27ce5819ed437c905975
Dan Holloran <dholloran@matchboxdesigngroup.com>
2015-01-26 15:53:59 -0600

    Sets page/single to use content-loop

--- snifter/page.php
+++ snifter/page.php
@@ -11,15 +11,4 @@
  * @since       Snifter 1.0.0
  */
 
-// Start the Loop.
-while ( have_posts() ) {
-	the_post();
-
-	// Include the page content template.
-	get_template_part( 'content', 'page' );
-
-	// If comments are open or we have at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) {
-		comments_template();
-	} // if()
-} // while()
+get_template_part( 'templates/content/content-loop' );
---
+++ snifter/templates/content/content-loop.php
@@ -0,0 +1,5 @@
+<div class="content-loop">
+<?php while ( have_posts() ) { ?>
+	<?php the_post(); ?>
+<?php } // while() ?>
+</div> <?php // .content-loop ?>
