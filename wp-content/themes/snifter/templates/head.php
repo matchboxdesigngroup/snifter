98df4b6d5c960b17b09f5ac4ac62fb7d381b79f9
Dan Holloran <dholloran@matchboxdesigngroup.com>
2015-07-17 09:23:03 -0500

    Fixes viewport scaling

--- wp-content/themes/snifter/templates/head.php
+++ wp-content/themes/snifter/templates/head.php
@@ -1,42 +1,49 @@
+<?php global $content_width; ?>
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
-<head>
-	<meta charset="utf-8">
-	<title><?php wp_title( '|', true, 'right' ); ?></title>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<!--[if IE 8]> <meta name="viewport" content="width=1170, maximum-scale=1.0, user-scalable=yes"> <![endif]-->
-	<!--[if gt IE 8]><!-->
-	<script>
-		var screenWidth = screen.width,
-				windowWidth = window.outerWidth,
-				respMeta    = '<meta name="viewport" content="width=device-width, initial-scale=1.0"/>',
-				noRespMeta  = '<meta name="viewport" content="width=1170, maximum-scale=1.0, user-scalable=yes">',
-				deviceWidth = 1170
-		;
+<!--[if gt IE 8]><!-->
+<html class="no-js" <?php language_attributes(); ?>>
+  <!--<![endif]-->
 
-		if (typeof screenWidth === 'undefined') {
-			deviceWidth = windowWidth;
-		} else if ( windowWidth < screenWidth ) {
-			deviceWidth = windowWidth;
-		} else {
-			deviceWidth = screenWidth;
-		} // if/elseif/else()
+  <head>
+    <meta charset="utf-8">
+    <title>
+      <?php wp_title('|', true, 'right'); ?>
+    </title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!--[if IE 8]> <meta name="viewport" content="width=1170, maximum-scale=1.0, user-scalable=yes"> <![endif]-->
+    <!--[if gt IE 8]><!-->
+    <script>
+      var screenWidth  = (typeof screen.width === 'undefined') ? 0 : screen.width,
+        windowWidth    = (typeof window.outerWidth === 'undefined') ? 0 : window.outerWidth,
+        deviceWidth    = <?php echo esc_attr($content_width); ?>,
+				mobileWidth    = 767,
+        respMeta       = '<meta name="viewport" content="width=device-width, initial-scale=1.0"/>',
+        noRespMeta     = '<meta name="viewport" content="width='+deviceWidth+', maximum-scale=1.0, user-scalable=yes">',
+        noWidthDefined = (screenWidth === 0 && windowWidth === 0);
 
-		// This allows the template to be responsive under 767 but not when above
-		if (typeof deviceWidth !== 'undefined' && deviceWidth > 767 ) {
-			// Sets the viewport to 1170
-			document.write(noRespMeta);
-		} else {
-			// Sets the viewport to device size
-			document.write(respMeta);
-		} // if/else()
-	</script>
-	<!--<![endif]-->
+      // Get the device width
+      if (screenWidth === 0 && !noWidthDefined) {
+        deviceWidth = windowWidth;
+      } else if (windowWidth === 0 && !noWidthDefined) {
+        deviceWidth = screenWidth;
+      } // if/elseif/else()
 
-	<?php wp_head(); ?>
+      // This allows the template to be responsive under 767 but not when above
+      if (deviceWidth > mobileWidth) {
+        // Sets the viewport to 1200
+        document.write(noRespMeta);
+      } else {
+        // Sets the viewport to device size
+        document.write(respMeta);
+      } // if/else()
 
-	<link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo( 'name' ); ?> Feed" href="<?php echo home_url(); ?>/feed/">
-</head>
+    </script>
+    <!--<![endif]-->
+
+    <?php wp_head(); ?>
+
+      <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
+  </head>
