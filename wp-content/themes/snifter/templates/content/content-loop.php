e266721aaac550e27f166cf406565b948cc4327b
Dan Holloran <dtholloran@gmail.com>
2015-09-13 19:17:54 -0500

    Fixes the loop missing the content closes #9

--- wp-content/themes/snifter/templates/content/content-loop.php
+++ wp-content/themes/snifter/templates/content/content-loop.php
@@ -1,6 +1,16 @@
+<?php
+/**
+ * Handles displaying the loop.
+ *
+ * @package     WordPress
+ * @subpackage  Snifter
+ * @since       Snifter 1.0.0
+ */
+
+?>
 <div class="content-loop fitvid">
 <?php while ( have_posts() ) { ?>
 	<?php the_post(); ?>
 	<?php the_content(); ?>
 <?php } // while() ?>
-</div> <?php // .content-loop ?>
+</div>
